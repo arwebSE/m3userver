c63f18c5d8b2cd859047126d334420bc76c1aa72
August M R <aggesoft@gmail.com>
2023-06-20 23:26:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const axios = require('axios');
 const fs = require('fs');
 const cron = require('node-cron');
-const request = require('request');
 
 const app = express();
 const port = 3000;
@@ -11,14 +10,18 @@ const epgUrl = process.env.EPG_URL;
 const downloadPath = './downloaded.m3u';
 const epgDownloadPath = './downloaded.xml';
 
-// Download file using request and store it on the server
-const downloadFile = (url, filePath) => {
-    return new Promise((resolve, reject) => {
-        request(url)
-            .pipe(fs.createWriteStream(filePath))
-            .on('finish', resolve)
-            .on('error', reject);
-    });
+// Download file using axios and store it on the server
+const downloadFile = async (url, filePath) => {
+    try {
+        const response = await axios.get(url, { responseType: 'stream' });
+        response.data.pipe(fs.createWriteStream(filePath));
+        return new Promise((resolve, reject) => {
+            response.data.on('end', resolve);
+            response.data.on('error', reject);
+        });
+    } catch (error) {
+        throw new Error(`Error downloading file from ${url}: ${error.message}`);
+    }
 };
 
 // Schedule the periodic download of the M3U and EPG files
@@ -48,6 +51,7 @@ app.all('/', (_req, res) => {
 
 // Serve the downloaded M3U file to any client that connects
 app.get('/m3u', (req, res) => {
+    console.log("Serving the m3u file...");
     if (!fs.existsSync(downloadPath)) {
         return res.status(500).send('M3U file not available');
     }
@@ -57,6 +61,7 @@ app.get('/m3u', (req, res) => {
 
 // Serve the downloaded EPG file to any client that connects
 app.get('/epg', (req, res) => {
+    console.log("Serving the epg file...");
     if (!fs.existsSync(epgDownloadPath)) {
         return res.status(500).send('EPG file not available');
     }
