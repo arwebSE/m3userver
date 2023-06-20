15ed9ffe8a7453796fa5ebfec58ead07cabbc04f
August M R <aggesoft@gmail.com>
2023-06-20 23:07:46 +0200

    Update index.js
    added epg dl

--- index.js
+++ index.js
@@ -2,31 +2,40 @@ const express = require('express');
 const axios = require('axios');
 const fs = require('fs');
 const cron = require('node-cron');
+const request = require('request');
 
 const app = express();
 const port = 3000;
 const m3uUrl = process.env.ENDPOINT_URL;
+const epgUrl = process.env.EPG_URL;
 const downloadPath = './downloaded.m3u';
+const epgDownloadPath = './downloaded.xml';
 
-// Download the M3U file and store it on the server
-const downloadM3U = async () => {
-    try {
-        const response = await axios.get(m3uUrl, { responseType: 'arraybuffer' });
-        fs.writeFileSync(downloadPath, Buffer.from(response.data, 'binary'));
-        console.log('M3U file downloaded successfully!');
-    } catch (error) {
-        console.error('Error downloading the M3U file:', error);
-    }
+// Download file using request and store it on the server
+const downloadFile = (url, filePath) => {
+    return new Promise((resolve, reject) => {
+        request(url)
+            .pipe(fs.createWriteStream(filePath))
+            .on('finish', resolve)
+            .on('error', reject);
+    });
 };
 
-// Schedule the periodic download of the M3U file
-cron.schedule('0 0 * * *', () => {
-    downloadM3U();
+// Schedule the periodic download of the M3U and EPG files
+cron.schedule('0 0 * * *', async () => {
+    try {
+        await downloadFile(m3uUrl, downloadPath);
+        console.log('M3U file downloaded successfully!');
+        await downloadFile(epgUrl, epgDownloadPath);
+        console.log('EPG file downloaded successfully!');
+    } catch (error) {
+        console.error('Error downloading files:', error);
+    }
 });
 
 // Ping route for uptimerobot
-app.all("/", (_req, res) => {
-    res.send("API is running!");
+app.all('/', (_req, res) => {
+    res.send('API is running!');
 });
 
 // Serve the downloaded M3U file to any client that connects
@@ -38,8 +47,16 @@ app.get('/m3u', (req, res) => {
     fileStream.pipe(res);
 });
 
+// Serve the downloaded EPG file to any client that connects
+app.get('/epg', (req, res) => {
+    if (!fs.existsSync(epgDownloadPath)) {
+        return res.status(500).send('EPG file not available');
+    }
+    const fileStream = fs.createReadStream(epgDownloadPath);
+    fileStream.pipe(res);
+});
+
 // Start the server
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-    downloadM3U();
 });
